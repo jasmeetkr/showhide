e35537d4f9905d3fb35513360b229804075a28ef
Jasmeet Kaur <jasmeet.kaur@dotvik.com>
2023-03-03 11:57:07 +0530

    react basics

---
+++ src/Default.js
@@ -0,0 +1,10 @@
+import react from 'react';
+
+export default function Default(){
+    return(
+        <div className='default-container'>
+            <h1>Welcome to React</h1>
+            <h2>Click below to toggle visibility</h2>
+        </div>
+    );
+}
